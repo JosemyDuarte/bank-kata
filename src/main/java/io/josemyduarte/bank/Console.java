06b88a6155ead34a14ccf81799a76bf63801752d
Josemy Duarte <duartejosemy@gmail.com>
2018-12-07 20:31:42 +0100

    Default implementation of console

--- src/main/java/io/josemyduarte/bank/Console.java
+++ src/main/java/io/josemyduarte/bank/Console.java
@@ -2,9 +2,6 @@ package io.josemyduarte.bank;
 
 public class Console {
     public void printLine(final String text) {
-
-        // TODO implement printLine
-        throw new UnsupportedOperationException();
-
+        System.out.println(text);
     }
 }
